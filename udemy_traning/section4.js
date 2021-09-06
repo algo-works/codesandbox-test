1f7cada19ac202d9a404d70726dd9127f3cd85f0
algo <30350083+algo-works@users.noreply.github.com>
2021-09-07 01:09:50 +0900

    section4-17を完了

---
+++ udemy_traning/section4.js
@@ -0,0 +1,107 @@
+/**
+ * Section4-18
+ * const, letなどの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const変数でもオブジェクトの場合はプロパティを変更できる
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28,
+// };
+// // console.log(val4);
+
+// val4.name = "jak";
+// console.log(val4);
+// val4.address = "Hiroshima";
+
+// const変数でも配列はプロパティを変更できる
+// 配列を確認
+const val5 = ["dog", "cat"];
+console.log(val5);
+
+// 配列を書き換え
+val5[0] = "bird";
+
+// 配列に要素を追加
+val5.push("monkey");
+/**
+ * const, letなどの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+// const変数でもオブジェクトの場合はプロパティを変更できる
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28,
+// };
+// // console.log(val4);
+
+// val4.name = "jak";
+// console.log(val4);
+// val4.address = "Hiroshima";
+
+// const変数でも配列はプロパティを変更できる
+// 配列を確認
+const val5 = ["dog", "cat"];
+console.log(val5);
+
+// 配列を書き換え
+val5[0] = "bird";
+
+// 配列に要素を追加
+val5.push("monkey");
